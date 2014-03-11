7025c4c9ba3a492c73848acf148c6d7c511f5e2d
Jack Markley <jack.markley@gmail.com>
2014-03-12 00:56:36 +0100

    adding migrate functionality

--- microblog.py
+++ microblog.py
@@ -1,11 +1,18 @@
 from flask import Flask
 from datetime import datetime
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.script import Manager
+from flask.ext.migrate import Migrate, MigrateCommand
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']="postgres:///microblog"
 db = SQLAlchemy(app)
 
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
 
 class BlagPost(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -29,4 +36,4 @@ def get_post(id):
     return BlagPost.query.filter_by(id=id).first()
 
 if __name__ == "__main__":
-    app.run()
+    manager.run()
