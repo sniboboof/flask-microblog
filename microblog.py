d2b966022e48da39956bcb972b3cf55fe7175ba9
Jack Markley <jack.markley@gmail.com>
2014-03-13 18:54:14 +0100

    some stuff works, still behind

--- microblog.py
+++ microblog.py
@@ -1,4 +1,6 @@
+import pdb
 from flask import Flask
+from jinja2 import Template
 from datetime import datetime
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.script import Manager
@@ -6,6 +8,7 @@ from flask.ext.migrate import Migrate, MigrateCommand
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']="postgres:///microblog"
+app.config['DEBUG'] = True
 db = SQLAlchemy(app)
 
 db = SQLAlchemy(app)
@@ -25,15 +28,50 @@ class BlagPost(db.Model):
         self.body = unicode(body)
         self.birthdate = posttime
 
+# class Author(db.Model):
+#     pass
+
 def write_post(title, body):
     db.session.add(BlagPost(title, body, datetime.now()))
     db.session.commit()
 
 def get_posts():
-    return BlagPost.query.all()
+    return BlagPost.query.order_by(BlagPost.birthdate.desc()).all()
 
 def get_post(id):
     return BlagPost.query.filter_by(id=id).first()
+
+@app.route('/')
+def posts_view():
+    myposts = get_posts()
+    viewpage = Template("""
+                        {% if myposts|length %}
+                            {% for post in myposts %}
+                                {{post.title}}<br/>{{post.birthdate}}<br/><br/>
+                            {% endfor %}
+                        {% else %}
+                            No posts yet
+                        {% endif %}
+                        """)
+    return viewpage.render(myposts=myposts)
+
+@app.route('/post/<int:postid>')
+def post_view(postid):
+    mypost = get_post(postid)
+    viewpage = Template("""
+                        {{title}}<br/>{{birthdate}}<br/>
+                        {{body}}<br/><br/>
+                        """)
+    try:
+        return viewpage.render(title=mypost.title,
+                               birthdate=mypost.birthdate,
+                               body=mypost.body)
+    except AttributeError:
+        return Template("That post hasn't been written yet!").render()
+
+@app.route('/write')
+def write_view(body):
+    pass
 
 if __name__ == "__main__":
     manager.run()
--- tests.py
+++ tests.py
@@ -1,12 +1,16 @@
 import microblog
 import unittest
+import os
 
 class testBlog(unittest.TestCase):
     def __init__(self, *args, **kwargs):
         super(testBlog, self).__init__(*args, **kwargs)
         microblog.app.config['SQLALCHEMY_DATABASE_URI']='postgres://postgres:@localhost/blagtest'
+        microblog.app.config['TESTING']=True
 
     def setUp(self):
+        microblog.app.config['TESTING'] = True
+        self.app = microblog.app.test_client()
         microblog.db.create_all()
 
     def tearDown(self):
@@ -29,8 +33,8 @@ class testBlog(unittest.TestCase):
         microblog.write_post("test title3", "test body3")
         a = microblog.get_posts()
 
-        self.assertEqual(a[0].title, "test title")
-        self.assertEqual(a[2].body, "test body3")
+        self.assertEqual(a[2].title, "test title")
+        self.assertEqual(a[0].body, "test body3")
 
     def testSingleGet(self):
         microblog.write_post("test title", "test body")
@@ -39,6 +43,40 @@ class testBlog(unittest.TestCase):
 
         self.assertEqual(microblog.get_post(1).title, "test title")
         self.assertEqual(microblog.get_post(3).body, "test body3")
+
+    def testPostsView(self):
+        rv = self.app.get('/')
+        assert 'No posts yet' in rv.data
+
+        microblog.write_post("test title", "test body")
+        microblog.write_post("test title2", "test body2")
+        microblog.write_post("test title3", "test body3")
+        rv = self.app.get('/')
+        assert 'test title' in rv.data
+        assert 'test title2' in rv.data
+        assert 'test title3' in rv.data
+
+    def testPostView(self):
+        rv = self.app.get('/post/1')
+        assert '404' in rv.data
+
+        microblog.write_post("test title", "test body")
+        microblog.write_post("test title2", "test body2")
+        microblog.write_post("test title3", "test body3")
+        rv = self.app.get('/post/1')
+        assert 'test title' in rv.data
+        assert 'test body' in rv.data
+        rv = self.app.get('/post/2')
+        assert 'test title2' in rv.data
+        assert 'test body2' in rv.data
+        rv = self.app.get('/post/3')
+        assert 'test title3' in rv.data
+        assert 'test body3' in rv.data
+        rv = self.app.get('/post/78')
+        assert '404' in rv.data
+
+    def testWriteView(self):
+        pass
 
 if __name__ == "__main__":
     unittest.main()
